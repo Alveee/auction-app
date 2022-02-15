acc8b8f0062453844a59dfb95243c4684679290b
Zahid Hasan Alvi <zahid.hasan@bs-23.net>
2022-02-15 22:31:20 +0600

    Refactor models

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -2,21 +2,21 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
-    {
-        name:  String,
-        email: String,
-        password: String,
-        maxBidAmount: Number,
-        bidAlertPercentage: Number,
-    },
-    {
-        timestamps: true
-    }
+  {
+    name: String,
+    email: String,
+    password: String,
+    maxBidAmount: Number,
+    bidAlertPercentage: Number,
+  },
+  {
+    timestamps: true,
+  }
 );
 
 /**
  * @typedef User
  */
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
